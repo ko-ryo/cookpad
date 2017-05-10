1774cfb29e8b42ab5b5509d02d1fd76f2c57ea30
ko-ryo <ryotak@RyotanoMacBook-Pro.local>
2017-05-10 21:42:47 +0900

    Edit files

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :application, "cookpad"
 set :repo_url, "git@github.com:ko-ryo/cookpad.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :pty, true
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'

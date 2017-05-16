2032cd345359d622ab53070760fb3105f080db4a
ko-ryo <ryotak@RyotanoMacBook-Pro.local>
2017-05-16 19:27:41 +0900

    Edit files

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,31 +1,31 @@
-$(function(){
+// $(function(){
 
-  function buildHTML(recipe){
-    var html = `
-      <div class="likes-counter">
-        <p>おすすめ！</p>
-        ${recipe.likes_count}
-      </div>`;
-    return html
-  };
+//   function buildHTML(recipe){
+//     var html = `
+//       <div class="likes-counter">
+//         <p>おすすめ！</p>
+//         ${recipe.likes_count}
+//       </div>`;
+//     return html
+//   };
 
-  $(function(){
-    $("#like-button").on("click", function(){
-      $.ajax({
-        type: 'POST',
-        url:'recipe_like_path',
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      })
-      .done(function(data) {
-        var html = buildHTML(recipe);
-        $("#like-button").html(buildHTML);
-      })
+//   $(function(){
+//     $("#like-button").on("click", function(){
+//       $.ajax({
+//         type: 'POST',
+//         url:'recipe_like_path',
+//         dataType: 'json',
+//         processData: false,
+//         contentType: false
+//       })
+//       .done(function(data) {
+//         var html = buildHTML(recipe);
+//         $("#like-button").html(buildHTML);
+//       })
       // .fail(function() {
       //   alert("エラーが発生しました");
       // });
-    });
-  });
-});
+//     });
+//   });
+// });
 

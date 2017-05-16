1f3ce776be98dcc8d176ec4270ad49e970fea473
ko-ryo <ryotak@RyotanoMacBook-Pro.local>
2017-05-16 19:23:23 +0900

    Edit files

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
 
-  before_action :move_to_index, except: :index
+  before_action :move_to_index, except: [:index,:show]
 
   def index
     @recipes = Recipe.includes(:user).page(params[:page]).per(5).order("created_at DESC")

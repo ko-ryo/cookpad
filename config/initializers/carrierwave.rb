a0dc2a5eb8c688d186b00ba6d6f7c4c276d1c8fd
ko-ryo <ryotak@RyotanoMacBook-Pro.local>
2017-05-18 19:20:51 +0900

    Edit carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,17 +4,16 @@ unless Rails.env.production?
   Dotenv.load
 end
 
-  CarrierWave.configure do |config|
-    config.fog_provider = 'fog'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: ENV['AWS_REGION']
-    }
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_REGION']
+  }
 
-    config.fog_directory = ENV['AWS_S3_BUCKET']
-    config.cache_storage = :fog
-    config.asset_host = ENV['S3_ASSET_HOST'] + '/' + ENV['AWS_S3_BUCKET']
-  end
+  config.fog_directory = ENV['AWS_S3_BUCKET']
+  config.cache_storage = :fog
+  config.asset_host = ENV['S3_ASSET_HOST'] + '/' + ENV['AWS_S3_BUCKET']
 end

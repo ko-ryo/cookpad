06214186c297d3c3a344231bf9c8ad7389dc61f2
ko-ryo <ryotak@RyotanoMacBook-Pro.local>
2017-05-19 18:26:53 +0900

    ユーザー個別ページの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,17 @@ class UsersController < ApplicationController
 
   def show
       @user = User.find(params[:id])
-      @favorite_recipe_ids = []
-      @favorite_records = Like.where(user_id: @user.id)
-      @favorite_records.each do |favorite_records|
-        recipe_id = favorite_records.recipe_id
-        @favorite_recipe_ids << recipe_id
-      end
+      get_favorite_recipe_ids
       @favorite_recipes = Recipe.where(id: @favorite_recipe_ids)
-      binding.pry
+      # binding.pry
   end
 
   def get_favorite_recipe_ids
+    @favorite_recipe_ids = []
+    @favorite_records = Like.where(user_id: @user.id)
+    @favorite_records.each do |favorite_records|
+      recipe_id = favorite_records.recipe_id
+      @favorite_recipe_ids << recipe_id
+    end
   end
 end

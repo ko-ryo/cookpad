33308384a770b524f13b553f72493e23a310fb41
ko-ryo <ryotak@RyotanoMacBook-Pro.local>
2017-05-22 12:40:51 +0900

    Edit carrierwave.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,11 +1,22 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
 
-# storage :file
-storage :fog
+storage :file
+# storage :fog
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
   end
 
 ####################################
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -19,25 +19,32 @@
 # end
 
 #######################################################################
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+# require 'carrierwave/storage/abstract'
+# require 'carrierwave/storage/file'
+# require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_REGION']
-  }
+# CarrierWave.configure do |config|
+#   config.storage = :fog
+#   config.fog_credentials = {
+#     provider: 'AWS',
+#     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+#     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+#     region: ENV['AWS_REGION']
+#   }
 
-  case Rails.env
-    when 'development'
-        config.fog_directory  = ENV['AWS_S3_BUCKET']
-        config.asset_host = ENV['S3_ASSET_HOST'] + '/' + ENV['AWS_S3_BUCKET']
-    when 'production'
-        config.fog_directory  = ENV['AWS_S3_BUCKET']
-        config.asset_host = ENV['S3_ASSET_HOST'] + '/' + ENV['AWS_S3_BUCKET']
-    end
-end
+#   case Rails.env
+#     when 'development'
+#         config.fog_directory  = ENV['AWS_S3_BUCKET']
+#         config.asset_host = ENV['S3_ASSET_HOST'] + '/' + ENV['AWS_S3_BUCKET']
+#     when 'production'
+#         config.fog_directory  = ENV['AWS_S3_BUCKET']
+#         config.asset_host = ENV['S3_ASSET_HOST'] + '/' + ENV['AWS_S3_BUCKET']
+#     end
+# end
+
+######################################################################
+
+
+# CarrierWave.configure do |config|
+#   config.asset_host = "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+# end

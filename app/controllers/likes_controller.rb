741d9a38b1c05b58ef914f117503e9a4b6cc2c0c
ko-ryo <ryotak@RyotanoMacBook-Pro.local>
2017-05-23 16:45:14 +0900

    おすすめボタンの編集

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,11 +2,13 @@ class LikesController < ApplicationController
   def create
     @like = Like.create(user_id: current_user.id, recipe_id: params[:recipe_id])
     @likes = Like.where(recipe_id: params[:recipe_id])
+    redirect_to root_path
   end
 
   def destroy
     @like = Like.find_by(user_id: current_user.id, recipe_id: params[:recipe_id])
     @like.destroy
     @likes = Like.where(recipe_id: params[:recipe_id])
+    redirect_to root_path
   end
 end
